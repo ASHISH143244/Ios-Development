2d08ae5ebca4cd80ee4f765ba7e13afb577b2d88
ASHISH143244 <79715499+ASHISH143244@users.noreply.github.com>
2023-07-13 11:18:10 +0530

    Multiplication Game Quiz

---
+++ Multiplication_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplication_gameApp.swift
+//  Multiplication_game
+//
+//  Created by ashish kumar dhimaan on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Multiplication_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
